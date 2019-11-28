0e833785730ff12320111d801e912b5c35b2e86a
stipaser <stipanovs@gmail.com>
2019-11-29 01:20:30 +0200

    Add project files.

---
+++ OpenTracingProj/Program.cs
@@ -0,0 +1,34 @@
+﻿using OpenTracing;
+using System.Threading;
+
+namespace OpenTracingProj
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Tracer.ConfigureTracer();
+
+         IScope scope = Tracer.Instance.BuildSpan("Main").WithTag("skills", "23").StartActive(true);
+         SomeMethod1();
+         Tracer.Instance.ScopeManager.Active?.Dispose();
+
+         var spans = Tracer.GetInMemorySpans();
+      }
+
+
+      static void SomeMethod1()
+      {
+         using(IScope scop = Tracer.Instance.BuildSpan("method22222").StartActive(true))
+         {
+            Thread.Sleep(2000);
+            SomeMethod2();
+         }        
+      }
+
+      static void SomeMethod2()
+      {
+         Thread.Sleep(1000);
+      }
+   }
+}
---
+++ OpenTracingProj/Tracer.cs
@@ -0,0 +1,39 @@
+﻿using OpenTracing;
+using OpenTracing.Util;
+using Jaeger;
+using Jaeger.Samplers;
+using Jaeger.Reporters;
+using Jaeger.Senders;
+using System.Collections.Generic;
+using JaegerTracer = Jaeger.Tracer;
+
+namespace OpenTracingProj
+{
+   public class Tracer
+   {
+      private static ITracer _tracer;
+      private static InMemoryReporter _inMemoryReporter;
+
+      public static void ConfigureTracer()
+      {
+         ISender sender = new HttpSender($"http://localhost:3526");
+         var reporter = new RemoteReporter.Builder().WithSender(sender).Build();
+
+         _inMemoryReporter = new InMemoryReporter();
+
+         _tracer = new JaegerTracer.Builder("Proscheduler")
+            .WithReporter(_inMemoryReporter)
+            .WithSampler(new ConstSampler(true))
+            .Build();
+
+         GlobalTracer.Register(_tracer);
+      }
+
+      public static ITracer Instance { get { return _tracer; } }
+
+      public static IReadOnlyList<Span> GetInMemorySpans() //also we can clear in memory list
+      {
+         return _inMemoryReporter?.GetSpans();
+      }
+   }
+}
